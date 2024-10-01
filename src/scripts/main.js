e5017033a88ef2fdb13dc232e0873fb619c1c399
CheryBerroa <chery.berroa@innovationcharter.org>
2024-10-01 15:50:06 +0000

    CB 10/1/24

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,8 @@ import "../styles/styles.css";
 console.log("What?");
 console.log("Hello world, from main.js!");
 if (typeof window !== "undefined") {
+  
+  
   // Put code that runs ONLY in the browser here -- this is most likely where
   // MOST of your code should go.
   //
