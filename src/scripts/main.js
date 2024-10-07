455676c8a92546245007afb6114c755773e1385a
CheryBerroa <chery.berroa@innovationcharter.org>
2024-10-07 16:24:33 +0000

    CB 10/7/24

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 import "../styles/styles.css";
-// Add imports here
-
+import Aos from "aos";
+import "aos/dist/aos.css";
+// import "@picocss/pico/css/pico.min.css";
 console.log("What?");
 console.log("Hello world, from main.js!");
 if (typeof window !== "undefined") {
@@ -8,6 +9,7 @@ if (typeof window !== "undefined") {
   
   // Put code that runs ONLY in the browser here -- this is most likely where
   // MOST of your code should go.
+  Aos.init();
   //
   // If you see a SSR error, try moving your code here!
   console.log("Hello world, browser :-)");
